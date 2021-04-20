b51d8745e0cf2d93e1549772c74add2cb69d71d9
darianhang <82900199+darianhang@users.noreply.github.com>
2021-04-20 15:10:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var Words = [
-  "Car", "Truck", "Back seat", "Front seat", "Mirror", "Wheels", "Tire", "Rim", "Bike", "Hill", "Mountain", "Phone", "Computer", "Monitor", "Keyboard", "Mouse", "Video Game", "Board Game", "TV", "Sofa", "Chair", "Music", "Headphones", "Speaker", "Cable", "Snake", "Dog", "Cat", "Cow", "Horse", "Disney", "Pixar", "Marvel", "DC", "ACDC", "Beatles", "Memes", "Flag", "Germany", "Canada", "France", "England", "Scotland", "USA", "Finland", "Switzerland", "Bus", "Plane", "Boat", "Sail Boat", "Speed Boat", "Moon", "Sun", "Mars", "Saturn", "Pluto", "Uranus", "Mercury", "Neptune", "Octopus", "Base Ball Hat", "Cowboy Hat", "Cherry Bush", "Cranberry Bush", "Apple Tree", "Starwars"," Tetris", "Pictionary", "Laptop", "PC", "GPU", "CPU", "cup", 
-];
-var RandomWord = Words[Math.floor(Math.random()*Words.length)];
-alert("Your word is: " + RandomWord);
-var Anwser;
-
 var canvas = document.querySelector("canvas")
 var ctx = canvas.getContext("2d");
 
