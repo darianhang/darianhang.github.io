883021ec31b1134e833631df79ede8c27d605787
darianhang <82900199+darianhang@users.noreply.github.com>
2021-08-11 15:27:42 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var Words = [
-  "Pod racing", "Ewoks", "Destroyer", "Death star", "Padme", "Master Yoda", "Chewbacca", "Jar Jar Binks", "Lightsaber", "Millennium Falcon", "Luke Skywalker", "Darth Vader", "Bobba Fett", "C-3PO", "R2-D2", "X-Wing", "Tyfighter", "Sand People", "Princess Leia", "Storm Trooper", "Kylo Ren", "Droid BB-8", "Jabba the Hut", "Darth Maul", "Han Solo", "Admiral Ackbar", "Mexico", "Mariachi", "Sombrero", "Maracas", "Cactus", "Pinata", "Burrito", "Taco", "Guacamole", "Salsa", "Cerveza", "Bouquet", "Candy", "Chocolate", "Family", "Gift", "Celebration", "Breakfast in bed", "Card", "Camping", "24 of Beer", "Beer pong", "Caesar (the drink)", "Queen Victoria", "Canadian Flag", "Maple Syrup", "Moose", "Butter Tarts", "Canadian Bacon (back bacon)", "Beaver", "Canadian Goose", "Mounties", "Hiking", "Toonie", "Canadian Rockies", "CN Tower", "Niagara Falls"
-];
-var RandomWord = Words[Math.floor(Math.random()*Words.length)];
-alert("Your word is: " + RandomWord);
-var Anwser;
